77ee1e7894287bf14643a4a1a20e5464b0699190
John McBride <jpmmcbride@gmail.com>
2022-08-05 21:05:26 -0600

    Automatically installs packer if not already installed
    Signed-off-by: John McBride <jpmmcbride@gmail.com>

--- lua/plugins/packer.lua
+++ lua/plugins/packer.lua
@@ -1,3 +1,10 @@
+local fn = vim.fn
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+if fn.empty(fn.glob(install_path)) > 0 then
+  packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+  vim.cmd [[packadd packer.nvim]]
+end
+
 return require('packer').startup(function()
 	-- Packer just manages itself
 	use 'wbthomason/packer.nvim'
@@ -99,4 +106,10 @@ return require('packer').startup(function()
 	-- Typescript. Oh Typescript. Where art thou Typescript.
 	use 'jose-elias-alvarez/null-ls.nvim'
 	use 'jose-elias-alvarez/nvim-lsp-ts-utils'
+
+  -- Automatically set up your configuration after cloning packer.nvim
+  -- Put this at the end after all plugins
+  if packer_bootstrap then
+    require('packer').sync()
+  end
 end)
